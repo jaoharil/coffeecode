4f6fa02355ef4e87957f2ff370b83a6239a1b303
jaoharil <jaoharilmaknun2904@gmail.com>
2024-06-05 14:18:14 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      // untuk tracking
+      letterSpacing: {
+        ngangkang: '.5em',
+      },
+    },
+  },
+  plugins: [],
+};
